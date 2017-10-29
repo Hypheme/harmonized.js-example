21745f1ab8041a48e15517757f47bf163230407f
JohannesMerz <johannes_merz@gmx.de>
2017-10-29 14:05:49 +0100

    travis ci integration

--- client/src/App.test.js
+++ client/src/App.test.js
@@ -1,8 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import renderer from 'react-test-renderer';
+
 import App from './App';
 
-it('renders without crashing', () => {
+test('renders without crashing', () => {
   const div = document.createElement('div');
   ReactDOM.render(<App />, div);
+});
+
+test('displays loading message', () => {
+  const app = renderer.create(<App/>);
+  expect(app).toMatchSnapshot();
 });
