3a4568a2430425512da94d3ee3913427d260862d
JohannesMerz <johannes_merz@gmx.de>
2017-10-29 16:25:49 +0100

    adds decorator support

--- client/src/todos/containers/ToDosView.js
+++ client/src/todos/containers/ToDosView.js
@@ -1,10 +1,10 @@
 import React from 'react';
+import { observer } from 'mobx-react';
 
-// @observer
-function ToDosView({ todos }) {
+const ToDosView = observer(({ todos }) => {
   return (
     <ul>{todos.map((todo, index) => <li key={index}>{todo.name}</li>)}</ul>
   );
-}
+});
 
 export default ToDosView;
--- client/src/todos/stores/toDosStore.js
+++ client/src/todos/stores/toDosStore.js
@@ -1,18 +1,21 @@
-import { Store, HttpTransporter, Schema } from 'harmonized';
-
-const toDosStore = new Store({
-  schema: new Schema({
-    task: String,
-    completed: String,
-    assignee: Number // todo add authors
-  }),
-  transporter: new HttpTransporter({
-    baseUrl: '/api',
-    path: 'todos'
-  })
-  // clientStorage: new LocalStorage({
-  //   // TODO: add options here when LocalStorage is implemented
-  // }),
-});
+// import { Store, HttpTransporter, Schema } from 'harmonized';
+//
+// const toDosStore = new Store({
+//   schema: new Schema({
+//     task: String,
+//     completed: String,
+//     assignee: Number // todo add authors
+//   }),
+//   transporter: new HttpTransporter({
+//     baseUrl: '/api',
+//     path: 'todos'
+//   })
+//   // clientStorage: new LocalStorage({
+//   //   // TODO: add options here when LocalStorage is implemented
+//   // }),
+// });
+const toDosStore = {
+  items: [{ name: 'hans' }]
+};
 
 export default toDosStore;
